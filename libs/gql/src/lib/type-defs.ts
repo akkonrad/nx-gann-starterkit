74b7091bbe136cdefb3decb892c021ded45a4d43
akkonrad <8158695+akkonrad@users.noreply.github.com>
2023-01-03 13:58:21 +0100

    updated model definition and relationships

--- libs/gql/src/lib/type-defs.ts
+++ libs/gql/src/lib/type-defs.ts
@@ -2,24 +2,25 @@
 import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
-  type Person {
-    name: String!
-    born: Int!
-    actedInMovies: [Movie!]!
-    @relationship(type: "ACTED_IN", properties: "ActedIn", direction: OUT)
-    directedMovies: [Movie!]! @relationship(type: "DIRECTED", direction: OUT)
-  }
+    type Person {
+        name: String!
+        born: Int
+        actedIn: [Movie!]! @relationship(type: "ACTED_IN", properties: "ActedIn", direction: OUT)
+        directed: [Movie!]! @relationship(type: "DIRECTED", direction: OUT)
+        produced: [Movie!]! @relationship(type: "PRODUCED", direction: OUT)
+    }
 
-  type Movie {
-    title: String!
-    released: Int!
-    actors: [Person!]!
-    @relationship(type: "ACTED_IN", properties: "ActedIn", direction: IN)
-    director: Person! @relationship(type: "DIRECTED", direction: IN)
-  }
+    type Movie {
+        title: String!
+        released: Int!
+        tagline: String
+        actors: [Person!]! @relationship(type: "ACTED_IN", properties: "ActedIn", direction: IN)
+        directedBy: [Person!]! @relationship(type: "DIRECTED", direction: IN)
+        producedBy: [Person!]! @relationship(type: "PRODUCED", direction: IN)
+    }
 
-  interface ActedIn @relationshipProperties {
-    roles: [String!]
-  }
+    interface ActedIn @relationshipProperties {
+        roles: [String!]
+    }
 
 `;
